40010660d1495e8a25a9220389c5696c9c9fdff9
Meena <aneemqah@gmail.com>
2024-12-27 21:32:02 +0000

    Fetch &  display data

--- fetchData.js
+++ fetchData.js
@@ -11,15 +11,36 @@ const fetchData = async (dataUrl) => {
   }
 };
 
-// Usage
-
-const displayProducts = async () => {
+// fetch all products from the API
+const fetchAllProducts = async () => {
   try {
     const products = await fetchData('https://dummyjson.com/products');
-    console.log(products);
+    // products object | products prop
+    if (products && products.products) {
+      return products.products;
+    } else {
+      console.error('No valid products found in the response.');
+      return [];
+    }
   } catch (error) {
     console.error('Error fetching products:', error);
   }
 };
 
-displayProducts();
+// Function to filter products with a rating above 4
+const filterHighRatedProducts = (products) => {
+  return products.filter((product) => product.rating > 4);
+};
+
+// Function to display high-rated products
+const displayHighRatedProducts = async () => {
+  try {
+    const allProducts = await fetchAllProducts();
+    const highRatedProducts = filterHighRatedProducts(allProducts);
+    console.log('Products with a rating above 4:', highRatedProducts);
+  } catch (error) {
+    console.error('Error displaying high-rated products:', error);
+  }
+};
+
+displayHighRatedProducts();
